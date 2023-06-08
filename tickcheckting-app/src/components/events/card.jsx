49113cd0b5873df8f10be221d17af89576b724c3
Carlos Estevez <54195505+Cestevez21@users.noreply.github.com>
2023-06-08 13:20:01 -0600

    Add: home responsivness

--- tickcheckting-app/src/components/events/card.jsx
+++ tickcheckting-app/src/components/events/card.jsx
@@ -1,23 +1,43 @@
 import React from "react";
+import { MapPinIcon } from "@heroicons/react/24/outline";
+import imgHolder from "../../images/images.jpg"
+const CardEvent = ({}) => {
 
-const CardEvent = ({key, image}) => {
+  //Variables que sera necesario pasar por prop:
+  var day = "12"
+  var month = "July"
+  var eventName = "Mago de Oz: Quinto aniversario"
+  //picture
+  var timeDate = "20:00"
+  var place = "Estadio Cuscatlan"
+  const routeHandler = (e) => {
 
-    console.log(key);
+  };
+  
   return (
-    
-    <div class="w-full bg-gray-900 rounded-lg sahdow-lg p-12 flex flex-col justify-center items-center">
-            <div class="mb-8">
+    <div className="w-full bg-gray-900 rounded-lg  p-5 flex flex-col justify-center items-center mb-8 relative">
+           
               <img
-                class="object-center object-cover h-36 w-36"
-                src={image} 
+                className="object-center object-cover h-64 w-44 rounded-3xl shadow-2xl"
+                src={imgHolder}
                 alt="photo"
               />
+              <div className="absolute h-12 w-12 right-28 bottom-9 lg:right-16 md:right-20   rounded-full bg-p-red text-p-white text-center text-xs font-bold uppercase justify-center flex flex-col">
+                <span>{month}</span>
+                <span>{day}</span>
+              </div>
+              {/* En la referencia debe ir el evento al que se va a hacer referencia una vez mas pasado por prop */}
+              <a className="absolute h-64 w-44 bg-p-brown bg-opacity-60 text-p-white rounded-3xl items-center flex flex-col text-center gap-3 justify-center uppercase px-5 font-black opacity-0 hover:opacity-100" onClick={routeHandler} >
+                  <span>{eventName}</span>
+                  <span className="capitalize">{day} {month}</span>
+                  <span className="normal-case">{timeDate} hrs.</span>
+                  <div className="flex flex-row items-center">
+                    <MapPinIcon className="w-6 h-6"/>
+                    <span>{place}</span>
+                  </div>
+              </a>
             </div>
-            <div class="text-center">
-              <p class="text-xl text-white font-bold mb-2">Jade Bradley</p>
-              <p class="text-base text-gray-400 font-normal">Dev Ops</p>
-            </div>
-    </div> );
+    );
         
     
   
