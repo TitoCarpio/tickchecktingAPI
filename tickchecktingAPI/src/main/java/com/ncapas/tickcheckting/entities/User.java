1233edec4eb4083ddcc9d9a36eadb60fa5a39e13
TitoCarpio <00226620@uca.edu.sv>
2023-06-08 20:26:53 -0600

    :sparkles: Add User entity

---
+++ tickchecktingAPI/src/main/java/com/ncapas/tickcheckting/entities/User.java
@@ -0,0 +1,50 @@
+package com.ncapas.tickcheckting.entities;
+
+//import java.util.List;
+import java.util.UUID;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+//import lombok.ToString;
+
+@Data
+//@ToString(exclude = "tokens")
+@NoArgsConstructor
+@Entity
+@Table(name = "user")
+public class User {
+	@Id
+	@Column(name = "code")
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private UUID code;
+	
+	@Column(name = "username")
+	private String username;
+	
+	@Column(name = "email")
+	private String email;	
+	
+	@Column(name = "password")
+	@JsonIgnore
+	private String password;
+	
+	@Column(name = "active", insertable = false)
+	private Boolean active;
+
+	public User(String username, String email, String password) {
+		super();
+		this.username = username;
+		this.email = email;
+		this.password = password;
+	}
+	
+	
+}
